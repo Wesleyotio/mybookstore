bcea0c5d6aa012e64b1a6f62b455c0cf744dad13
José Wesley Gonçalves <wesleymagnos@hotmail.com>
2023-06-24 10:59:24 -0300

    create routes for API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login',  [UserController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function() {
+    Route::post('/logout', [UserController::class, 'logout']);
+    Route::post('/user/update', [UserController::class, 'update']);
 });
+Route::middleware('auth:sanctum')->get('/books', 'Controller@index');
+Route::middleware('auth:sanctum')->post('/books', 'BookController@create');
+Route::middleware('auth:sanctum')->put('/books/{id}', 'BookController@update');
+Route::middleware('auth:sanctum')->delete('/books/{id}', 'BookController@delete');
